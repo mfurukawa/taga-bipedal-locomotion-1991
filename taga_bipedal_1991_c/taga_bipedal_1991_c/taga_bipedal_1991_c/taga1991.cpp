c82044d8c31faa5b1ed5b62d5147cb3ff080152b
Masahiro Furukawa (3A1) <m.furukawa@ist.osaka-u.ac.jp>
2016-10-13 14:35:29 +0900

    start debugging

--- taga_bipedal_1991_c/taga_bipedal_1991_c/taga_bipedal_1991_c/taga1991.cpp
+++ taga_bipedal_1991_c/taga_bipedal_1991_c/taga_bipedal_1991_c/taga1991.cpp
@@ -7,27 +7,3 @@
 //
 // rev 0.0, Created,  Oct 4, 2016-
 
-#include "taga1991.h"
-
-
-void draw() {
-	// (6) derivered from Matsuoka 1985,1987 and Grillner 1981.
-
-	for (int i = 1; i <= 12; i++) {
-		y[i] = f(u[i]);
-
-		print(u[i] + "\t");
-
-		u_d[i] = -u[i] - beta*v[i] + u[0] + Feed[i];
-		v_d[i] = -v[i] + y[i];
-
-		for (int j = 1; j <= 12; j++)
-			u_d[i] = u_d[i] + w[i][j] * y[j];
-
-		u_d[i] = u_d[i] / tau[i];
-		v_d[i] = u_d[i] / taud[i];
-	}
-
-
-
-}
