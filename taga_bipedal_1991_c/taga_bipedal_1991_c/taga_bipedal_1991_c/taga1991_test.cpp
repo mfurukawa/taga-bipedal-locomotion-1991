adc5fdabf2ca58704b430d77c5a32da53b44da76
Masahiro Furukawa (3A1) <m.furukawa@ist.osaka-u.ac.jp>
2016-10-13 14:47:06 +0900

    start debugging "taga1991"

---
+++ taga_bipedal_1991_c/taga_bipedal_1991_c/taga_bipedal_1991_c/taga1991_test.cpp
@@ -0,0 +1,26 @@
+/* (c) Masahiro Furukawa
+*
+* m.furukawa@ist.osaka-u.ac.jp
+*
+* Created Oct 11, 2016
+*
+* Description
+* - gauss-jordan method test
+*
+* referece:
+* http://www.yamamo10.jp/yamamoto/lecture/2006/5E/Linear_eauations/gaussj.pdf
+*/
+
+#include <stdio.h>
+#include "gauss_jordan.h"
+#include "taga1991.h"
+
+int main() {
+
+	Taga1991 taga1991;
+
+	taga1991.next();
+
+	return 0;
+}
+
